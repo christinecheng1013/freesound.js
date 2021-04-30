a62f401bb8fff6111fd9b94fb6220ee6aff3a798
christinecheng1013 <christinecheng1013@gmail.com>
2021-04-30 05:25:04 -0500

    Add freesound API

--- samplers.js
+++ samplers.js
@@ -1,10 +1,14 @@
 
-import * as Tone from "tone";
+
+
+// import * as Tone from "tone";
+let file = "";
 
 const players = new Tone.Players({
     sample1: "https://christinecheng1013.github.io/NetArt-Repo/media/chime1.wav",
     sample2: "https://christinecheng1013.github.io/NetArt-Repo/media/chime2.wav",
-    sample3: "https://christinecheng1013.github.io/NetArt-Repo/media/chime3.wav"
+    sample3: "https://christinecheng1013.github.io/NetArt-Repo/media/chime3.wav",
+    sample4: `${file}`
 });
 
 let slider1 = document.getElementById("slider1");
@@ -14,6 +18,12 @@ let slider4 = document.getElementById("slider4");
 let slider5 = document.getElementById("slider5");
 let slider6 = document.getElementById("slider6");
 let slider7 = document.getElementById("slider7");
+
+//attach a click listener to a play button
+// document.querySelector("#onoff")?.addEventListener("click", async () => {
+//     await Tone.start();
+//     console.log("audio is ready");
+// });
 
 let chorus = new Tone.Chorus({
     frequency: 1.5,
@@ -35,15 +45,19 @@ let phaser = new Tone.Phaser({
 });
 phaser.toDestination();
 
-slider1.onclick = () => {
+
+// slider1.value = 1;
+
+slider1.oninput = () => {
     chorus.frequency.value = slider1.value * 0.7;
+    console.log(slider1.value);
 };
 
-slider2.onclick = () => {
+slider2.onchange = () => {
     chorus.delayTime = slider2.value * 1.3;
 };
 
-slider3.onclick = () => {
+slider3.onchange = () => {
     chorus.depth = slider3.value * 0.1;
 };
 slider4.onclick = () => {
@@ -87,3 +101,64 @@ document.querySelector("#onoff")?.addEventListener("click", async () => {
     console.log("audio is ready");
 });
 
+
+window.onload = function(){
+    freesound.setToken("Sa2fz69rAvDoTQ94BoizW82lEnz1nDqF9GlP2wrT");
+}
+let soundfile = document.getElementById('soundfile');
+let searchInput = document.getElementById('searchTerm');
+
+searchInput.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    console.log("submit");
+    findSounds();
+    event.preventDefault();
+  }
+})
+
+function findSounds(searchWord) {
+  if(!searchWord){
+      searchWord = document.getElementById('searchTerm').value;
+  }
+  console.log("find: ", searchWord)
+
+  freesound.textSearch(searchWord, {}, function(sounds){
+      console.log(sounds)
+      let soundID = sounds.results[0].id;
+      getSound(soundID);
+  })
+}
+
+function getSound(soundID){
+  freesound.getSound(soundID, function(sound){
+      console.log(sound);
+      file = sound.previews['preview-lq-mp3'];
+      console.log("file: ", file)
+      soundfile.src = file;
+
+
+
+      players.sample4 = `${file}`;
+      console.log(file, players.sample4);
+
+      const players2 = new Tone.Players({
+        sample4: `${file}`
+    });
+    
+    players2.connect(chorus);
+    players2.connect(distortion);
+    players2.connect(phaser);
+    
+    document.getElementById("buttonD").addEventListener("click", () => {
+        players2.player("sample4").start();
+    });
+    
+
+    
+    //   players[sample1] = file;    
+    //   document.getElementById("buttonA").addEventListener("click", () => {
+    //     players.player("sample4").start();
+    // });
+   
+  })
+}
